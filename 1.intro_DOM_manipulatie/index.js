f5ef5552c09dbf4609558de057ac7c1d3d167f3e
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-12-02 10:25:28 +0100

    starting javascript dom

---
+++ 1.intro_DOM_manipulatie/index.js
@@ -0,0 +1,5 @@
+console.log(test);
+//1. hoe kan ik elementen selecten
+//2. hoe kan ik gebeurtenis  van de gebruiker opvangen
+//3. hoe kan ik styling van elementen manipuleren
+//4. hoe kan ik html injecten/manipuleren?
