09c6531a8fc590856ed16fed1bede88b05c83fbb
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-12-02 10:40:56 +0100

    added the readme file

--- 1.intro_DOM_manipulatie/index.js
+++ 1.intro_DOM_manipulatie/index.js
@@ -1,5 +1,15 @@
-console.log(test);
+console.log("test");
 //1. hoe kan ik elementen selecten
+console.log(document.getElementById("paragraph"));
+console.log(document.getElementsByClassName("test"));
+//HTML collection => [i] length
+const liMetClassTest = document.getElementsByClassName("test");
+for (let i = 0; i < liMetClassTest.length; i++) {
+  console.log(liMetClassTest[i]);
+}
+document.querySelector("ul");
+//nodelist
+document.querySelectorAll("ul li");
 //2. hoe kan ik gebeurtenis  van de gebruiker opvangen
 //3. hoe kan ik styling van elementen manipuleren
 //4. hoe kan ik html injecten/manipuleren?
