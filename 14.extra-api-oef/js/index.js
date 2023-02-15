6755a890af3e7d06baa6b3d290e2f73a3ad6353d
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2023-02-15 09:33:01 +0100

    updates

--- 14.extra-api-oef/js/index.js
+++ 14.extra-api-oef/js/index.js
@@ -1,3 +1,31 @@
 import '../css/style.scss';
+import axios from 'axios';
 
-console.log('test');
+/* VARS */
+const form = document.querySelector('form');
+const grid = document.querySelector('.cocktailgrid');
+const cocktailCardTemplate = document.getElementById('cocktailcard').innerHTML;
+const searchInput = document.querySelector('form input');
+
+//on form submit
+form.onsubmit = async (e) => {
+  //stop refresh
+  e.preventDefault();
+  const {
+    data: { drinks },
+  } = await axios(
+    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' +
+      searchInput.value
+  );
+  //doorloop de data array met een map
+  grid.innerHTML = drinks
+    .map(({ strDrink, strDrinkThumb }) =>
+      cocktailCardTemplate
+        .replace('%IMG%', 'strDrinkThumb')
+        .replaceAll('%COCKTAIL%', 'strDrink')
+    )
+    .join('');
+};
+
+//add event listener
+// form.addEventListener("submit",()=>{})
